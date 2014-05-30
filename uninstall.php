a56564e4ca128ce1a3ec7249bf6117f129661703
zohaibmir <zohaib.mir@gmail.com>
2014-05-30 10:29:18 +0200

    fix uninstall plugin issue

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,8 @@
 <?php
-
+define( 'MRS1_PLUGIN_DIR', plugin_dir_path(__FILE__));
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
-require_once plugins_url('', __FILE__) . '/classes/UnInstallPlugin.php';
+include_once( MRS1_PLUGIN_DIR . 'classes/UnInstallPlugin.php');
 UnInstallPlugin::init();
 ?>
