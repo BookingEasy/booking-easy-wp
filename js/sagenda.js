eebd72257d656671239d5f01f415376661bf5b8a
zohaibmir <zohaib.mir@gmail.com>
2014-09-23 08:14:00 +0200

    Front-end css modifications

--- js/sagenda.js
+++ js/sagenda.js
@@ -15,39 +15,83 @@ jQuery(document).ready(function($) {
     }
     
     
-    //Date Pickers  
+//    //Date Pickers  
+//    var nowTemp = new Date();
+//    var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+//    
+//    var checkin = $('#dpd1').datepicker({
+//        format: 'mm-dd-yyyy',
+//        onRender: function(date) {
+//            return date.valueOf() < now.valueOf() ? 'disabled' : '';
+//        }
+//    }).on('changeDate', function(ev) {
+//        $("#bookableitems option[value='0']").attr('selected', 'selected');
+//        if (ev.date.valueOf() > checkout.date.valueOf()) {
+//            var newDate = new Date(ev.date)
+//            newDate.setDate(newDate.getDate() + 1);
+//            checkout.setValue(newDate);
+//            $("#bookableitems option[value='0']").attr('selected', 'selected');            
+//        }
+//        checkin.hide();
+//        $('#dpd2')[0].focus();
+//    }).data('datepicker');
+//    var checkout = $('#dpd2').datepicker({
+//        format: 'mm-dd-yyyy',
+//        onRender: function(date) {
+//            return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+//        }
+//    }).on('changeDate', function(ev) {
+//        $("#bookableitems option[value='0']").attr('selected', 'selected');
+//        checkout.hide();
+//    }).data('datepicker');
+//    
+    
+    // New Code
     var nowTemp = new Date();
     var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-    
-    var checkin = $('#dpd1').datepicker({
+     var checkin = $('#dp4').datepicker({
         format: 'mm-dd-yyyy',
         onRender: function(date) {
             return date.valueOf() < now.valueOf() ? 'disabled' : '';
         }
-    }).on('changeDate', function(ev) {
-        $("#bookableitems option[value='0']").attr('selected', 'selected');
-        if (ev.date.valueOf() > checkout.date.valueOf()) {
+    })
+    .on('changeDate', function(ev){
+        if (ev.date.valueOf() > endDate.valueOf()){            
             var newDate = new Date(ev.date)
             newDate.setDate(newDate.getDate() + 1);
             checkout.setValue(newDate);
-            $("#bookableitems option[value='0']").attr('selected', 'selected');            
+            $("#bookableitems option[value='0']").attr('selected', 'selected');  
         }
-        checkin.hide();
-        $('#dpd2')[0].focus();
-    }).data('datepicker');
-    var checkout = $('#dpd2').datepicker({
+        else {         
+            startDate = new Date(ev.date);
+            $('#dpd1').val($('#dp4').data('date'));
+        }
+        $('#dp4').datepicker('hide');
+    $('#dp5')[0].focus();
+    });
+    $('#dp5').datepicker({
         format: 'mm-dd-yyyy',
         onRender: function(date) {
             return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
         }
-    }).on('changeDate', function(ev) {
-        $("#bookableitems option[value='0']").attr('selected', 'selected');
-        checkout.hide();
-    }).data('datepicker');
+    })
+    .on('changeDate', function(ev){
+        
+        if (ev.date.valueOf() < startDate.valueOf()){
+            
+        } else {
+            $("#bookableitems option[value='0']").attr('selected', 'selected');
+            endDate = new Date(ev.date);
+            $('#dpd2').val($('#dp5').data('date'));
+        }
+        $('#dp5').datepicker('hide');
+    });
     
     //Bookable Item Change
     
     $("#bookableitems").change(function() {
+        
+       
         if(this.value == 0) {
             alert("Please Select a Bookable Item to see the Events");            
         }       
@@ -58,8 +102,8 @@ jQuery(document).ready(function($) {
                 dataType: 'html',
                 data: {
                     action: 'getEventsList',
-                    startDate: formatDate(checkin.date),
-                    endDate:formatDate(checkout.date),
+                    startDate: $('#dpd1').val(),
+                    endDate:$('#dpd2').val(),
                     bookableItemId:$('#bookableitems :selected').val()
                 },
                 success: function(data) {
--- templates/reservation.php
+++ templates/reservation.php
@@ -1,57 +1,77 @@
-<div class="container1">
-    <div class="row">
-        <div class="col-xs-12">
-            <p class="text-warning">
-                <?php
-                if (!is_user_logged_in()) {
-                    echo '<p>You need to be a site member to be able to ';
-                    echo 'Subscribe for events. Sign up to gain access!</p>';
-                    return;
-                }
-                ?>
-            </p>
-        </div>
+<div class="sagenda_container">
+    <div class="sagenda_col">
+
+        <p class="sagenda_text-warning">
+            <?php
+            if (!is_user_logged_in()) {
+                echo '<p>You need to be a site member to be able to ';
+                echo 'Subscribe for events. Sign up to gain access!</p>';
+                return;
+            }
+            ?>
+        </p>
     </div>
-    <div class="row">
-        <div class="col-xs-12">
+
+    <div class="sagenda_row">
+        <div class="sagenda_col">
             <form method="post" id="subscribe_event" action="" role="form">                
                 <div id="form-step1">
-                    <div class="row">
-                        <div class="col-xs-12">
+                    <div class="sagenda_row">
+                        <div class="sagenda_col">
                             <?php wp_nonce_field('add_reservation_form', 'br_user_form'); ?>
-                            <div class="alert alert-success" style="margin-left: 0;display: none">You successfully subscribe for the event.
+                            <div class="sagenda_alert sagenda_alert-success" style="margin-left: 0;display: none">
+                                You successfully subscribe for the event.
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-xs-12">
+                    <div class="sagenda_row">
+                        <div class="sagenda_col">
+
+                            <div class="well">
+            
+            
+			<div class="alert alert-error" id="alert">
+				<strong>Oh snap!</strong>
+			  </div>
+			<table class="table">
+				
+				<tbody>
+					<tr>
+						<td id="startDate">2012-02-20</td>
+						<td id="endDate">2012-02-25</td>
+					</tr>
+				</tbody>
+			</table>
+          </div>
                             
-                            <div class="form-group">
+                            <div class="sagenda_form-group">
                                 <label for="exampleInputEmail1">Start date</label>
-                                 <div class="input-append date" id="dp1" data-date="12-02-2012" data-date-format="dd-mm-yyyy">                                
-                                    <input type="text" class="form-control" data-val="true" readonly value="" id="dpd1">
-                                    <span class="add-on"><i class="icon-th"></i></span>
+                                <div class="sagenda_input-append date" id="dp1" data-date="12-02-2012" data-date-format="dd-mm-yyyy">                                
+                                    <input type="text" class="sagenda_form-control"  readonly value="" id="dpd1">
+                                    <a href="#" class="sagenda_add-on" id="dp4" data-date-format="dd-mm-yyyy" data-val="true" data-date=""><i class="sagenda_icon-th"></i></a>
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="form-group">
+                    <div class="sagenda_row">
+                        <div class="sagenda_col">
+                            <div class="sagenda_form-group">
                                 <label for="exampleInputEmail1">End Date</label>
-                                <div class="input-append date" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
-                                    <input type="text" class="form-control" value="" readonly id="dpd2">
-                                    <span class="add-on"><i class="icon-th"></i></span>
+                                <div class="sagenda_input-append date" id="dp3" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
+                                    <input type="text" class="sagenda_form-control" value="" readonly id="dpd2">
+                                    <a href="#" class="sagenda_add-on" id="dp5" data-date-format="dd-mm-yyyy" data-val="true" data-date=""><i class="sagenda_icon-th"></i></a>
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="form-group">
+                    <div class="sagenda_row">
+                        <div class="sagenda_col">
+                            <div class="sagenda_form-group">
                                 <label for="exampleInputPassword1">Bookable Items</label>
                                 <br />
-                                <select class="form-control " id="bookableitems" >
+                                <select class="sagenda_form-control " id="bookableitems" >
                                     <option value="0">Bookable Item</option>
                                     <?php foreach ($bookableItems as $bookableItem) { ?>
                                         <option value="<?php echo $bookableItem->Id ?>"><?php echo $bookableItem->Name; ?></option>
@@ -60,9 +80,9 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="form-group">
+                    <div class="sagenda_row">
+                        <div class="sagenda_col">
+                            <div class="sagenda_form-group">
                                 <label for="exampleInputPassword1">Click an event to book It:</label>
                                 <br />
                                 <div id="events-list">
@@ -75,3 +95,5 @@
             </form>
         </div>
     </div>
+
+</div>
