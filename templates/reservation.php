6e8d188dc35c1e7ee31622f3393021951978d677
zohaibmir <zohaib.mir@gmail.com>
2014-09-28 03:27:50 +0200

    Fixed module for older versions

--- templates/reservation.php
+++ templates/reservation.php
@@ -1,25 +1,18 @@
 <div class="sagenda_container">
     <div class="sagenda_col">
 
-        
-            <?php
-            if (!$connected) {
-                echo '<div class="sagenda_alert sagenda_alert-faliure">';
-                echo '<p>You didn’t connect your Sagenda account :</p>';
-                echo '<p>1.&nbsp; Create a free account on <a href="https://sagenda.net/Accounts/Register">Sagenda.</a> (setup your “bookable items” and events).</p>';
-                echo '<p>2.&nbsp; Copy your token (from the backend of sagenda.net Settings / account settings) to your WordPress installation (backend of wp / Settings / Sagenda).</p>';
-                echo '</div>';
-            }
-            /*if (!is_user_logged_in()) {
-                echo '<p>You need to be a site member to be able to ';
-                echo 'Subscribe for events. Sign up to gain access!</p>';
-                return;
-            }*/
-            
-            
-            ?>
-        
-        
+
+        <?php
+        if (!$connected) {
+            echo '<div class="sagenda_alert sagenda_alert-faliure">';
+            echo '<p>You didn’t connected your Sagenda account :</p>';
+            echo '<p>1.&nbsp; Create a free account on <a href="https://sagenda.net/Accounts/Register">Sagenda.</a> (setup your “bookable items” and events).</p>';
+            echo '<p>2.&nbsp; Copy your token (from the backend of sagenda.net Settings / account settings) to your WordPress installation (backend of wp / Settings / Sagenda).</p>';
+            echo '</div>';
+        }
+        ?>
+
+
     </div>
 
     <div class="sagenda_row">
@@ -34,33 +27,61 @@
                             </div>
                         </div>
                     </div>
-                    <div class="sagenda_row">
-                        <div class="sagenda_col">
-                            <div class="sagenda_alert sagenda_alert-error" id="alert">
+                    <?php if ($this->version > 3.4) { ?>
+                        <div class="sagenda_row">
+                            <div class="sagenda_col">
+                                <div class="sagenda_alert sagenda_alert-error" id="alert">
 
-                            </div>
-                            <div class="sagenda_form-group">
-                                <label for="exampleInputEmail1">Start date</label>
-                                <div class="sagenda_input-append date" id="startDate" data-date="12-02-2012" data-date-format="dd-mm-yyyy">                                
-                                    <input type="text" class="sagenda_form-control"  readonly value="" id="dpd1">
-                                    <div class="sagenda_add-on" id="dp4" data-date-format="dd-mm-yyyy" data-val="true" data-date=""><i class="sagenda_icon-th"></i></div>
+                                </div>
+                                <div class="sagenda_form-group">
+                                    <label for="exampleInputEmail1">Start date</label>
+                                    <div class="sagenda_input-append date" id="startDate" data-date="12-02-2012" data-date-format="dd-mm-yyyy">                                
+                                        <input type="text" class="sagenda_form-control"  readonly value="" id="dpd1">
+                                        <div class="sagenda_add-on" id="dp4" data-date-format="dd-mm-yyyy" data-val="true" data-date=""><i class="sagenda_icon-th"></i></div>
 
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="sagenda_row">
-                        <div class="sagenda_col">
-                            <div class="sagenda_form-group">
-                                <label for="exampleInputEmail1">End Date</label>
-                                <div class="sagenda_input-append date" id="endDate" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
-                                    <input type="text" class="sagenda_form-control" value="" readonly id="dpd2">
-                                    <div class="sagenda_add-on" id="dp5" data-date-format="dd-mm-yyyy" data-val="true" data-date=""><i class="sagenda_icon-th"></i></div>
+                        <div class="sagenda_row">
+                            <div class="sagenda_col">
+                                <div class="sagenda_form-group">
+                                    <label for="exampleInputEmail1">End Date</label>
+                                    <div class="sagenda_input-append date" id="endDate" data-date="12-02-2012" data-date-format="dd-mm-yyyy">
+                                        <input type="text" class="sagenda_form-control" value="" readonly id="dpd2">
+                                        <div class="sagenda_add-on" id="dp5" data-date-format="dd-mm-yyyy" data-val="true" data-date=""><i class="sagenda_icon-th"></i></div>
 
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+
+                        <?php
+                    } else {
+                        ?>
+                        <div class="sagenda_row">
+                            <div class="sagenda_col">
+                                <div class="sagenda_form-group">
+                                    <label for="exampleInputEmail1">Start Date</label>
+                                    <div>
+                                        <input type="date" id="startDate"  name="startDate" class="sagenda_form-control" value="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="sagenda_row">
+                            <div class="sagenda_col">
+                                <div class="sagenda_form-group">
+                                    <label for="exampleInputEmail1">End Date</label>
+                                    <div>
+                                        <input type="date" id="endDate"  name="endDate" class="sagenda_form-control" value="" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    <?php } ?>
                     <div class="sagenda_row">
                         <div class="sagenda_col">
                             <div class="sagenda_form-group">
