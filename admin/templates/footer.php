714b53961cd1d9c0078f213895e42721e7f0cf66
David Imboden <david.imboden@iteration.info>
2015-03-18 12:55:04 +0100

    update new wording references

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -1,4 +1,5 @@
 <br style="clear:both;" />
-<p class="help">Add this tag [sagenda-wp] in your pages, posts or in template.</p>
-<p class="help">**NOTE: You first need to register on Sagenda and then you will get a Authentication token which you will use to validate this Sagenda Plugin. **</p>
-<p class="help">To learn more about our booking easy service please visit : <a href="http://www.sagenda.com/">www.sagenda.com</a></p>
+<p class="help"><?php _e('**NOTE: You first need to register on Sagenda and then you will get a Authentication token which you will use to validate this Sagenda Plugin. **', 'sagenda-wp'); ?></p>
+<p class="help"><?php _e('You can read more information about <a href="http://www.sagenda.com/add-booking-system-website/add-booking-plugin-wordpress-website/">Sagenda WordPress plugin</a>', 'sagenda-wp'); ?></p>
+<p class="help"><?php _e('To create a free account : <a href="https://sagenda.net/Accounts/Login">https://sagenda.net/Accounts/Login</a>', 'sagenda-wp'); ?></p>
+
