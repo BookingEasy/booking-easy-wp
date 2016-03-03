0d1b4e88c77349255573ffb7cf03eba957d43dc6
David Imboden <david.imboden@iteration.info>
2016-03-03 17:56:13 +0100

    update for reading .mo . mo files
    Some kind of tricks to make Herbert read /language folder.

--- app/Controllers/SearchController.php
+++ app/Controllers/SearchController.php
@@ -2,10 +2,15 @@
 
 //use Herbert\Framework\Models\Post;
 
+
 class SearchController {
 
-    public function showSearch()
-    {
-        return view('@Sagenda/search.twig');
-    }
+  public function showSearch()
+  {
+    return view('@Sagenda/search.twig',
+    [
+      'title'         => __( 'Please fill out all the required fields', 'sagenda-wp' )
+    ]
+  );
+}
 }
--- plugin.php
+++ plugin.php
@@ -1,15 +1,24 @@
 <?php
 
 /**
- * @wordpress-plugin
- * Plugin Name:       Sagenda test on Herbert framework
- * Plugin URI:        http://plugin-name.com/
- * Description:       A plugin.
- * Version:           1.0.0
- * Author:            Sagenda
- * Author URI:        http://author.com/
- * License:           MIT
- */
+* @wordpress-plugin
+* Plugin Name:       Sagenda
+* Plugin URI:        http://plugin-name.com/
+* Description:       A plugin.
+* Version:           1.2.0 - Test rebuild on Herbet
+* Author:            Sagenda
+* Author URI:        http://author.com/
+* License:           MIT
+* Domain Path:        /languages
+*/
+
+/*
+* Translations
+*/
+add_action( 'plugins_loaded', 'my_herbert_plugin_load_textdomain' );
+function my_herbert_plugin_load_textdomain() {
+  load_plugin_textdomain( 'sagenda-wp', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
